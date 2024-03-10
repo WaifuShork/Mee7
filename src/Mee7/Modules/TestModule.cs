7ee2628b9b65744b89e1e6927a75d6ad960cec1b
atraks1swife <atraks1swife@proton.me>
2024-03-10 12:23:44 -0400

    added slash command execution

---
+++ src/Mee7/Modules/TestModule.cs
@@ -0,0 +1,13 @@
+﻿using Discord.Interactions;
+
+namespace Mee7.Modules;
+
+public class TestModule : InteractionModuleBase<ShardedInteractionContext>
+{
+    [SlashCommand("ping", "pong!")]
+    public async Task PingAsync()
+    {
+        // Context.Interaction.
+        await Context.Interaction.RespondAsync("Pong!");
+    }
+}
--- src/Mee7/Services/CommandHandler.cs
+++ src/Mee7/Services/CommandHandler.cs
@@ -1,10 +1,13 @@
 ﻿using System.Reflection;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Discord.Addons.Hosting;
+using Discord.Addons.Hosting.Util;
+using Discord.Interactions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
-using Serilog;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mee7.Services;
 
@@ -13,41 +16,20 @@ namespace Mee7.Services;
 // Discord.NET docs: https://discordnet.dev/guides/introduction/intro.html
 // Basic Commands: https://discordnet.dev/guides/text_commands/intro.html
 // Slash Commands: https://discordnet.dev/guides/int_basics/application-commands/intro.html
-public class CommandHandler(
-    DiscordShardedClient client,
-    ILogger<DiscordShardedClientService> logger,
-    IServiceProvider provider,
-    CommandService commandService,
-    AlphaConfig alphaConfig)
-    : DiscordShardedClientService(client, logger)
+public class CommandHandler(DiscordShardedClient client, ILogger<DiscordShardedClientService> logger, IServiceProvider provider, CommandService commandService, InteractionService interactionService, AlphaConfig alphaConfig) : DiscordShardedClientService(client, logger)
 {
-    private readonly IServiceProvider m_provider = provider;
-    private readonly CommandService m_commandService = commandService;
-    private readonly AlphaConfig m_alphaConfig = alphaConfig;
-    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        Log.Information("Initializing Command Handler");
-        Log.Information($"Look a token: '{m_alphaConfig.Token}'");
-        
-        Client.MessageCommandExecuted += OnMessageCommandExecutedAsync; // Message based command
-        Client.SlashCommandExecuted += OnSlashCommandExecutedAsync;     // Application slash command, these can be more involved
-        
-        // Adds all classes marked as ModuleBase, remember to use `ModuleBase<ShardedCommandContext>`
-        await m_commandService.AddModulesAsync(Assembly.GetEntryAssembly(), m_provider);
-    }
-
-    // Normal Command Executed
-    private Task OnMessageCommandExecutedAsync(SocketMessageCommand arg)
-    {
-        Log.Information("Message Command {CommandName}", arg.CommandName);
-        return Task.CompletedTask;
-    }
-
-    // Slash Command Executed
-    private Task OnSlashCommandExecutedAsync(SocketSlashCommand arg)
-    {
-        Log.Information("Slash Command {CommandName}", arg.CommandName);
-        return Task.CompletedTask;
+        await Client.WaitForReadyAsync(stoppingToken).ContinueWith(async _ =>
+        {
+            await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), provider).ConfigureAwait(false);
+            await interactionService.RegisterCommandsGloballyAsync().ConfigureAwait(false);
+            Client.InteractionCreated += async interaction =>
+            {
+                var scope = provider.CreateAsyncScope();
+                var ctx = new ShardedInteractionContext(Client, interaction);
+                await interactionService.ExecuteCommandAsync(ctx, scope.ServiceProvider).ConfigureAwait(false);
+            };
+        }, stoppingToken).ConfigureAwait(false);
     }
 }
